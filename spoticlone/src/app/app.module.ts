69525fe88fd072dd4bec7dc45da30a1c616511fc
hrdax <juanpa_200018@hotmail.com>
2023-08-29 18:56:28 -0400

    eliminado favoritepagecomponent que daba error

--- spoticlone/src/app/app.module.ts
+++ spoticlone/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FavoritePageComponent } from './modules/favorites/pages/favorite-page/favorite-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FavoritePageComponent
+    
   ],
   imports: [
     BrowserModule,
