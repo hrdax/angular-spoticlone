d95d8319dbb25deb6041e783b5eb22066a21ef16
hrdax <juanpa_200018@hotmail.com>
2023-08-29 18:56:57 -0400

    entregando data a trackmodel para visualizar

--- spoticlone/src/app/shared/components/play-list-body/play-list-body.component.ts
+++ spoticlone/src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from '../../../data/tracks.json'
+import { TrackModel } from '@core/models/tracks.models';
 
 @Component({
   selector: 'app-play-list-body',
   templateUrl: './play-list-body.component.html',
   styleUrls: ['./play-list-body.component.css']
 })
-export class PlayListBodyComponent {
+export class PlayListBodyComponent implements OnInit{
+  tracks: TrackModel[] = []
 
+  ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default
+    this.tracks = data;
+  }
 }
