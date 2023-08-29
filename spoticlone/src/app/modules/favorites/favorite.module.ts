bf15a3eccd00f1f7073eef5428b73b15d6924937
hrdax <juanpa_200018@hotmail.com>
2023-08-29 18:37:14 -0400

    importando sharedmodule

--- spoticlone/src/app/modules/favorites/favorite.module.ts
+++ spoticlone/src/app/modules/favorites/favorite.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FavoriteRoutingModule } from './favorite-routing.module';
+import { SharedModule } from '@shared/shared.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FavoriteRoutingModule
+    FavoriteRoutingModule,
+    SharedModule
   ]
 })
 export class FavoriteModule { }
