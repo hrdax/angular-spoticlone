e1fdc61b9f3868620c5d3fccc9586209c2644225
hrdax <juanpa_200018@hotmail.com>
2023-08-29 17:15:35 -0400

    routes home routing

--- spoticlone/src/app/modules/home/home-routing.module.ts
+++ spoticlone/src/app/modules/home/home-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { TracksModule } from '@modules/tracks/tracks.module';
+import { FavoriteModule } from '@modules/favorites/favorite/favorite.module';
+import { HistoryModule } from '@modules/history/history.module';
 
 
 const routes: Routes = [
   {
-    path: '',
-    component: HomePageComponent,
+    path: 'tracks',
+    loadChildren: () => import('@modules/tracks/tracks.module').then(m => TracksModule)
+  },
+  {
+    path: 'favorites',
+    loadChildren: () => import('@modules/favorites/favorite.module').then(m => FavoriteModule)
+  },
+  {
+    path: 'history',
+    loadChildren: () => import('@modules/history/history.module').then(m => HistoryModule)
   }
 ];
 
