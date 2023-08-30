861d4715f632de445f1070a5e995ae393b650ec4
hrdax <juanpa_200018@hotmail.com>
2023-08-30 18:49:35 -0400

    refactorizando a un observable

--- spoticlone/src/app/modules/history/pages/history-page/history-page.component.ts
+++ spoticlone/src/app/modules/history/pages/history-page/history-page.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.models';
 import { SearchService } from '@modules/history/services/search.service';
-
+import { Observable, of } from 'rxjs'
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html',
   styleUrls: ['./history-page.component.css']
 })
 export class HistoryPageComponent {
-  listResults: TrackModel[] = []
+  listResults$: Observable<any> = of([])
   constructor(private searchService: SearchService) {}
 
 
   recieveData(event: string): void {
-    this.searchService.searchTracks$(event)
-      .subscribe(({ data }) => {
-        this.listResults = data 
-      })
+    this.listResults$ = this.searchService.searchTracks$(event)
   }
 
 
