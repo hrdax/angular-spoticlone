758a58b4de6c768d4bd9e27d4cbfd9ca49509095
hrdax <juanpa_200018@hotmail.com>
2023-08-30 15:54:06 -0400

    uso api get

--- spoticlone/src/app/modules/tracks/services/track.service.ts
+++ spoticlone/src/app/modules/tracks/services/track.service.ts
@@ -1,30 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.models';
 import { Observable, of } from 'rxjs'
-import * as dataRaw from '../../../data/tracks.json'
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackService {
 
-  TracksMetal$: Observable<TrackModel[]> = of([])
-  TracksRandom$: Observable<TrackModel[]> = of([])
+  private readonly URL = environment.api
 
 
-  constructor() { 
-    const { data }: any = (dataRaw as any).default
-    this.TracksMetal$ = of(data)
-
-    this.TracksRandom$ = new Observable((observer) => {
-      const trackExample: TrackModel = {
-        _id: 9,
-        name: 'll',
-        album:'ll',
-        url: 'http://',
-        cover: 'http://'
-      }
-      observer.next([trackExample])
-    })
+  constructor(private httpClient:HttpClient) { 
+    
+  }
+  getAllTracks$(): Observable<any> {
+      return this.httpClient.get(`${this.URL}/tracks`)
   }
 }
