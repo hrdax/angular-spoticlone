1517176da7a4e6221655ed743d23f4df74661968
hrdax <juanpa_200018@hotmail.com>
2023-08-30 14:17:14 -0400

    validando formlario de login

--- spoticlone/src/app/modules/auth/pages/login-page/login-page.component.ts
+++ spoticlone/src/app/modules/auth/pages/login-page/login-page.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms'
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css']
 })
-export class LoginPageComponent {
+export class LoginPageComponent implements OnInit {
+  FormLogin: FormGroup = new FormGroup([])
+
+  ngOnInit(): void {
+    this.FormLogin = new FormGroup(
+      {
+        email: new FormControl('',[
+          Validators.required,
+          Validators.email
+        ]),
+        password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(6),
+          Validators.maxLength(12)
+        ])
+      }
+    )
+  }
 
 }
