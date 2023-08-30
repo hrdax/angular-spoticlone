4e2096bca8dbd3dcfa5f0ca16cc96fd4b00cebe2
hrdax <juanpa_200018@hotmail.com>
2023-08-29 21:49:48 -0400

    utilizacion router en sidebar

--- spoticlone/src/app/shared/components/side-bar/side-bar.component.ts
+++ spoticlone/src/app/shared/components/side-bar/side-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
@@ -13,7 +14,7 @@ export class SideBarComponent implements OnInit {
 
   customOptions: Array<any> = []
 
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.mainMenu.defaultOptions = [
