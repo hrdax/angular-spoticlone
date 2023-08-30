abb5c2f41e79cfd06cf0e4443db9d4b0ebf2c551
hrdax <juanpa_200018@hotmail.com>
2023-08-29 22:08:39 -0400

    directiva imgbroken

---
+++ spoticlone/src/app/shared/directives/img-broken.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: 'img[appImgBroken]'
+})
+export class ImgBrokenDirective {
+  @HostListener('error') handleError(): void {
+    const elNative = this.elHost.nativeElement
+    elNative.src = '../../../assets/images/img-broken.jpg'
+  }
+
+  constructor(private elHost: ElementRef) { 
+
+  }
+
+}
