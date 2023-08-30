f7baa8b7df32d43908f3332a680b28874f8f5e99
hrdax <juanpa_200018@hotmail.com>
2023-08-30 15:54:19 -0400

    get all tracks subscribe

--- spoticlone/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ spoticlone/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import * as dataRaw from '../../../../data/tracks.json'
 import { TrackModel } from '@core/models/tracks.models';
 import { TrackService } from '@modules/tracks/services/track.service';
 import { Subscription } from 'rxjs'
@@ -20,23 +19,14 @@ export class TracksPageComponent implements OnInit, OnDestroy{
   }
 
   ngOnInit(): void {
-    const observer1$ = this.tracksService.TracksMetal$
-    .subscribe(response => {
-      this.TracksMetal = response
-      this.TracksRandom = response
-      console.log(response)
-    })
-
-    const observer2$ = this.tracksService.TracksRandom$
-    .subscribe(response => {
-      this.TracksRandom = [...this.TracksRandom, ...response]
-      console.log('cancion random',response)
-    })
-    this.listObservers$ = [observer1$, observer2$]
+    this.tracksService.getAllTracks$()
+      .subscribe(response => {
+        console.log('aaa', response)
+      })
   }
 
   ngOnDestroy(): void {
-    this.listObservers$.forEach(u => u.unsubscribe())
+    
   }
 
 }
