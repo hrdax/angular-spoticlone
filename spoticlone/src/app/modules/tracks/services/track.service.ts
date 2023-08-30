863d7c9c15da074ad2b70eb733b0cee06c7fb678
hrdax <juanpa_200018@hotmail.com>
2023-08-30 16:18:51 -0400

    obetniendo data y probando filtros en allrandom

--- spoticlone/src/app/modules/tracks/services/track.service.ts
+++ spoticlone/src/app/modules/tracks/services/track.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { TrackModel } from '@core/models/tracks.models';
 import { Observable, of } from 'rxjs'
 import { environment } from 'src/environments/environment';
+import { map, mergeMap } from 'rxjs/operators'
+import { TrackModel } from '@core/models/tracks.models';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +16,33 @@ export class TrackService {
   constructor(private httpClient:HttpClient) { 
     
   }
+
+  private skipById(listTracks:TrackModel[], id: number): Promise<TrackModel[]> {
+    return new Promise((resolve, reject) => {
+      const listTmp = listTracks.filter(a => a._id != id)
+      resolve(listTmp)
+    })
+  }
+
   getAllTracks$(): Observable<any> {
       return this.httpClient.get(`${this.URL}/tracks`)
+        .pipe(
+          map(({data}: any) => {
+            return data
+          })
+        )
   }
+
+  getAllRandom$(): Observable<any> {
+    return this.httpClient.get(`${this.URL}/tracks`)
+      .pipe(
+        mergeMap(({data}: any) => this.skipById(data, 1)),
+        // map(({data}: any) => {
+        //   return data.reverse()
+        // }),
+        // map((dataReverted: any) => { // probando filtro id
+        //   return dataReverted.filter((track: TrackModel) => track._id != 7)
+        // })
+      )
+}
 }
