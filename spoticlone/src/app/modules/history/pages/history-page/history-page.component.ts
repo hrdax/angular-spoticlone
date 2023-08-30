de622c09099468e5957bb56e12559175174a0643
hrdax <juanpa_200018@hotmail.com>
2023-08-30 18:43:15 -0400

    dando resultados a listresults

--- spoticlone/src/app/modules/history/pages/history-page/history-page.component.ts
+++ spoticlone/src/app/modules/history/pages/history-page/history-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.models';
 import { SearchService } from '@modules/history/services/search.service';
 
 @Component({
@@ -7,14 +8,14 @@ import { SearchService } from '@modules/history/services/search.service';
   styleUrls: ['./history-page.component.css']
 })
 export class HistoryPageComponent {
-  
+  listResults: TrackModel[] = []
   constructor(private searchService: SearchService) {}
 
 
   recieveData(event: string): void {
     this.searchService.searchTracks$(event)
-      .subscribe(res => {
-        
+      .subscribe(({ data }) => {
+        this.listResults = data 
       })
   }
 
