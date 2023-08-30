99c97dccff635dd309d3009b0a12a14153306260
hrdax <juanpa_200018@hotmail.com>
2023-08-30 15:02:13 -0400

    observer

--- spoticlone/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ spoticlone/src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,20 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import * as dataRaw from '../../../../data/tracks.json'
 import { TrackModel } from '@core/models/tracks.models';
+import { TrackService } from '@modules/tracks/services/track.service';
+import { Subscription } from 'rxjs'
 
 @Component({
   selector: 'app-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
-export class TracksPageComponent implements OnInit{
-  mockTracksList: Array<TrackModel> = [
+export class TracksPageComponent implements OnInit, OnDestroy{
+  TracksMetal: Array<TrackModel> = []
+  TracksElectro: Array<TrackModel> = []
 
-  ]
+  listObservers$: Array<Subscription> = []
+
+  constructor(private tracksService: TrackService){
+
+  }
 
   ngOnInit(): void {
-    const { data }: any = (dataRaw as any).default
-    this.mockTracksList = data;
+    const observer1$ = this.tracksService.TracksMetal$
+    .subscribe(response => {
+      this.TracksMetal = response
+      console.log(response)
+    })
+    this.listObservers$ = [observer1$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe())
   }
 
 }
