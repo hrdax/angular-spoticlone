7f592b72cc4de5795487bf73061db4d30c24bbbd
hrdax <juanpa_200018@hotmail.com>
2023-08-30 19:36:36 -0400

    dando play a los tracks

--- spoticlone/src/app/shared/services/multimedia.service.ts
+++ spoticlone/src/app/shared/services/multimedia.service.ts
@@ -1,4 +1,6 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.models';
+import { BehaviorSubject, Observable, Observer, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,7 +9,24 @@ export class MultimediaService {
 
   callback: EventEmitter<any> = new EventEmitter<any>()
 
-  constructor() { }
+  public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined)
 
+  public audio!: HTMLAudioElement
+
+  constructor() {
+    this.audio = new Audio()
+    this.trackInfo$.subscribe(responseok => {
+    this.setAudio(responseok)
+    })
+  }
+
+  private listenAllEvents(): void {
+
+  }
+
+  public setAudio(track: TrackModel): void {
+    this.audio.src = track.url
+    this.audio.play()
+  }
 
 }
