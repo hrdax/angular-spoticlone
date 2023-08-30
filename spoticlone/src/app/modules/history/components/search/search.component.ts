f30739d21c768b85af56064a448a7a973ffc9c9a
hrdax <juanpa_200018@hotmail.com>
2023-08-30 18:38:28 -0400

    search component

---
+++ spoticlone/src/app/modules/history/components/search/search.component.ts
@@ -0,0 +1,20 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent {
+  
+  @Output() callbackData: EventEmitter<any> = new EventEmitter()
+
+  src: string = ''
+
+  callSearch(term: string): void {
+    if (term.length > 3) {
+      this.callbackData.emit(term)
+    }
+  }
+
+}
