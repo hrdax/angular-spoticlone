5980e810643387665261e51d55a189d57c36ff6a
hrdax <juanpa_200018@hotmail.com>
2023-08-30 19:13:25 -0400

    tracks test

--- node-api-tracks/app/controlles/tracks.js
+++ node-api-tracks/app/controlles/tracks.js
@@ -4,8 +4,41 @@ const PORT = process.env.PORT || 3000
 const URL_PUBLIC = process.env.URL_PUBLIC || '/'
 const getItems = async(req, res) => {
     try {
-        const listAll = [{
+        const listAll = [
+            {
                 "_id": 1,
+                "name": "Visions",
+                "album": "There Is A Hell Believe Me I've Seen It. There Is A Heaven Let's Keep It A Secret",
+                "cover": "https://i5.walmartimages.com/seo/There-Is-A-Hell-Believe-Me-I-ve-Seen-It-There-Is-A-Heaven-Let-s-Keep-It-A-Secret-CD_77b9a7d8-3c5e-4329-ac9d-a04bf59b1cf2_1.12db58afe9183f503089ce50d1c9f639.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+                "artist": {
+                    "name": "Bring Me The Horizon",
+                    "nickname": "Bring Me The Horizon",
+                    "nationality": "UK"
+                },
+                "duration": {
+                    "start": 0,
+                    "end": 333
+                },
+                "url": `${URL_PUBLIC}/track-visions.mp3`
+            },
+            {
+                "_id": 2,
+                "name": "Aligator Blood",
+                "album": "There Is A Hell Believe Me I've Seen It. There Is A Heaven Let's Keep It A Secret",
+                "cover": "https://i5.walmartimages.com/seo/There-Is-A-Hell-Believe-Me-I-ve-Seen-It-There-Is-A-Heaven-Let-s-Keep-It-A-Secret-CD_77b9a7d8-3c5e-4329-ac9d-a04bf59b1cf2_1.12db58afe9183f503089ce50d1c9f639.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
+                "artist": {
+                    "name": "Bring Me The Horizon",
+                    "nickname": "Bring Me The Horizon",
+                    "nationality": "UK"
+                },
+                "duration": {
+                    "start": 0,
+                    "end": 333
+                },
+                "url": `${URL_PUBLIC}/track-alligatorblood.mp3`
+            },
+            {
+                "_id": 3,
                 "name": "Forsaken",
                 "album": "Systematic Chaos",
                 "cover": "https://upload.wikimedia.org/wikipedia/en/e/e3/Dream_Theater_-_Systematic_Chaos.jpg",
@@ -21,7 +54,7 @@ const getItems = async(req, res) => {
                 "url": `${URL_PUBLIC}/track-forsaken.mp3`
             },
             {
-                "_id": 2,
+                "_id": 4,
                 "name": "Glass Heart",
                 "album": "Lost Souls",
                 "cover": "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/b2/d3/7e/b2d37e39-2aa7-d5ce-060e-7aed6fb92b97/cover.jpg/600x600bf-60.jpg",
@@ -37,7 +70,7 @@ const getItems = async(req, res) => {
                 "url": `${URL_PUBLIC}/track-glassheart.mp3`
             },
             {
-                "_id": 3,
+                "_id": 5,
                 "name": "Drowned in Emotion",
                 "album": "Lost Souls",
                 "cover": "https://is1-ssl.mzstatic.com/image/thumb/Music124/v4/b2/d3/7e/b2d37e39-2aa7-d5ce-060e-7aed6fb92b97/cover.jpg/600x600bf-60.jpg",
@@ -53,7 +86,7 @@ const getItems = async(req, res) => {
                 "url": `${URL_PUBLIC}/track-drowned.mp3`
             },
             {
-                "_id": 4,
+                "_id": 6,
                 "name": "Like a Villain",
                 "album": "THE DEATH OF PEACE OF MIND",
                 "cover": "https://upload.wikimedia.org/wikipedia/en/thumb/5/54/The_Death_of_Peace_of_Mind_Bad_Omens.png/220px-The_Death_of_Peace_of_Mind_Bad_Omens.png",
@@ -69,7 +102,7 @@ const getItems = async(req, res) => {
                 "url": `${URL_PUBLIC}/track-likeavillain.mp3`
             },
             {
-                "_id": 5,
+                "_id": 7,
                 "name": "Limits",
                 "album": "Finding God Before God Finds Me",
                 "cover": "https://is1-ssl.mzstatic.com/image/thumb/Music122/v4/bd/02/4e/bd024ede-f9f4-cc4d-c7c7-a58dbb45b1f5/22UM1IM05474.rgb.jpg/1200x1200bb.jpg",
@@ -85,23 +118,7 @@ const getItems = async(req, res) => {
                 "url": `${URL_PUBLIC}/track-limits.mp3`
             },
             {
-                "_id": 6,
-                "name": "Aligator Blood",
-                "album": "There Is A Hell Believe Me I've Seen It. There Is A Heaven Let's Keep It A Secret",
-                "cover": "https://i5.walmartimages.com/seo/There-Is-A-Hell-Believe-Me-I-ve-Seen-It-There-Is-A-Heaven-Let-s-Keep-It-A-Secret-CD_77b9a7d8-3c5e-4329-ac9d-a04bf59b1cf2_1.12db58afe9183f503089ce50d1c9f639.jpeg?odnHeight=768&odnWidth=768&odnBg=FFFFFF",
-                "artist": {
-                    "name": "Bring Me The Horizon",
-                    "nickname": "Bring Me The Horizon",
-                    "nationality": "UK"
-                },
-                "duration": {
-                    "start": 0,
-                    "end": 333
-                },
-                "url": `${URL_PUBLIC}/track-5.mp3`
-            },
-            {
-                "_id": 7,
+                "_id": 8,
                 "name": "Falling Away From Me",
                 "album": "Issues",
                 "cover": "https://upload.wikimedia.org/wikipedia/en/b/b6/KoRnIssues.jpg",
@@ -117,7 +134,7 @@ const getItems = async(req, res) => {
                 "url": `${URL_PUBLIC}/track-fallingaway.mp3`
             },
             {
-                "_id": 8,
+                "_id": 9,
                 "name": "Time of Dying",
                 "album": "One-X",
                 "cover": "https://upload.wikimedia.org/wikipedia/en/c/cf/One_X.png",
@@ -131,7 +148,8 @@ const getItems = async(req, res) => {
                     "end": 333
                 },
                 "url": `${URL_PUBLIC}/track-timeof.mp3`
-            }
+            },
+           
         ]
         res.send({ data: listAll })
     } catch (e) {
