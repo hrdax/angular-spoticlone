db0c44636c12153921d1d0141810b87856f57540
hrdax <juanpa_200018@hotmail.com>
2023-08-30 18:08:29 -0400

    utilizando el interceptor para la app

--- spoticlone/src/app/app.module.ts
+++ spoticlone/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CookieService } from 'ngx-cookie-service';
+import { InjectSessionInterceptor } from '@core/interceptors/inject-session.interceptor';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,15 @@ import { CookieService } from 'ngx-cookie-service';
     HttpClientModule
     
   ],
-  providers: [CookieService],
+  providers: 
+  [
+    CookieService,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InjectSessionInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
