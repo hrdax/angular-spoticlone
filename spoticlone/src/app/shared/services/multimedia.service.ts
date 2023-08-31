7b081a2d1257f109c80b96f9f620e4883f655b0a
hrdax <juanpa_200018@hotmail.com>
2023-08-31 13:25:12 -0400

    time elapsed and remaining

--- spoticlone/src/app/shared/services/multimedia.service.ts
+++ spoticlone/src/app/shared/services/multimedia.service.ts
@@ -12,15 +12,50 @@ export class MultimediaService {
   public trackInfo$: BehaviorSubject<any> = new BehaviorSubject(undefined)
 
   public audio!: HTMLAudioElement
+  public timeElapsed$: BehaviorSubject<string> = new BehaviorSubject('00:00')
+  public timeRemaining$: BehaviorSubject<string> = new BehaviorSubject('-00:00')
 
   constructor() {
     this.audio = new Audio()
     this.trackInfo$.subscribe(responseok => {
     this.setAudio(responseok)
     })
+
+    this.listenAllEvents()
   }
 
   private listenAllEvents(): void {
+    this.audio.addEventListener('timeupdate', this.calculateTime, false)
+
+  }
+
+  private calculateTime = () => {
+    const { duration, currentTime } = this.audio
+    this.setTimeElapsed(currentTime)
+    this.setRemaining(currentTime, duration)
+  }
+  
+  private setTimeElapsed(currentTime: number): void {
+    let seconds = Math.floor(currentTime % 60)
+    let minutes = Math.floor((currentTime / 60) % 60)
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes
+    const displayFormat = `${displayMinutes}:${displaySeconds}`
+    
+    this.timeElapsed$.next(displayFormat)
+
+  }
+
+  private setRemaining(currentTime: number, duration: number): void {
+    let timeLeft = duration - currentTime
+    let seconds = Math.floor(timeLeft % 60)
+    let minutes = Math.floor((timeLeft / 60) % 60)
+
+    const displaySeconds = (seconds < 10) ? `0${seconds}` : seconds
+    const displayMinutes = (minutes < 10) ? `0${minutes}` : minutes
+    const displayFormat = `-${displayMinutes}:${displaySeconds}`
+    this.timeRemaining$.next(displayFormat)
 
   }
 
