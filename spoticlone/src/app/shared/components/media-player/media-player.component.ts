3f3c91ee9573d36806226e67f9a87ee4d93c740d
hrdax <juanpa_200018@hotmail.com>
2023-08-31 13:39:32 -0400

    stado de pausa

--- spoticlone/src/app/shared/components/media-player/media-player.component.ts
+++ spoticlone/src/app/shared/components/media-player/media-player.component.ts
@@ -16,9 +16,13 @@ export class MediaPlayerComponent implements OnInit, OnDestroy{
   }
 
   listObservers$: Array<Subscription> = []
+  state: string = 'paused'
 
   ngOnInit(): void {
+    const observer1$ = this.multimediaService.playerStatus$
+      .subscribe(status => this.state = status)
     
+    this.listObservers$ = [observer1$]
     
   }
 
